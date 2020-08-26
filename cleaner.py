79422e8e79ab2fe97331344731ad42fe7429b1b5
Andrey <rav.0318@gmail.com>
2020-08-26 10:57:09 +0400

    Fixed logging to the file

--- cleaner.py
+++ cleaner.py
@@ -1,5 +1,5 @@
 import os, os.path, shutil
-outF = open('myOutFile.txt', 'w')
+outF = open('Log.txt', 'w')
 rmvfile = 'remove.txt'
 if os.path.exists(rmvfile) and os.path.isfile(rmvfile):
     path = input("Please enter the path to root directory to start with clean-up:\n")
@@ -10,11 +10,17 @@ if os.path.exists(rmvfile) and os.path.isfile(rmvfile):
             if os.path.exists(path+line.strip()):
                 if os.path.isfile(path+line.strip()):
                     os.remove(path+line.strip())
-                    print('File ', line.strip(), ' has been removed')
+                    print("File", line.strip(), "has been removed")
+                    logmsg="File "+line.strip()+" has been removed"
+                    outF.write(logmsg)
                 if os.path.isdir(path+line.strip()):
                     shutil.rmtree(path+line.strip())
-                    print('Directory ', line.strip(), ' has been removed')
+                    print("Directory", line.strip(), "has been removed")
+                    logmsg="Directory "+line.strip()+" has been removed"
+                    outF.write(logmsg)
             else:
-                print(line.strip(), ' does not exist in your cleaner utility directory')
+                print(line.strip(), "does not exist in your cleaner utility directory")
+                logmsg = line.strip() + " does not exist"
+                outF.write(logmsg)
             line = fp.readline()
             cnt += 1
